4f046c37cad3850b40b272312f4378cab6e50c0f
Astijojo <58069656+Astijojo@users.noreply.github.com>
2020-02-24 23:34:38 +0100

    scripLookat
    création d'un script de suivie des loups

---
+++ Assets/Scripts/wolfScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfScript : MonoBehaviour
+{
+
+    private Transform player;
+    public float moveSpeed;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        player = GameObject.FindWithTag("Player").transform;
+        transform.LookAt(player);
+        transform.position = Vector3.Lerp(transform.position, player.position, 0.01f);
+    }
+}
