6eb13456af9b1ca19b17eee3b4b0abcd2858ec32
Aid0z <mutheleteric@gmail.com>
2020-02-25 18:40:01 +0100

    Added animator for the wolf

--- Assets/Scripts/wolfScript.cs
+++ Assets/Scripts/wolfScript.cs
@@ -6,14 +6,15 @@ public class wolfScript : MonoBehaviour
 {
 
     private Transform player;
+    [SerializeField] private Animator animation;
     public float moveSpeed;
-    
+    private static readonly int Moving = Animator.StringToHash("moving");
 
 
     // Update is called once per frame
     void Update()
     {
-
+        animation.SetBool(Moving, true);
         player = GameObject.FindWithTag("Player").transform;
         transform.LookAt(player);
         transform.position = Vector3.Lerp(transform.position, player.position, 0.01f);
