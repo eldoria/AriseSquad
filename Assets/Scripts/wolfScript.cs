61f42f816319d04e183205a563e1ac5382194ba0
Aid0z <mutheleteric@gmail.com>
2020-02-25 19:14:56 +0100

    Wolves actions depending on distance with player

--- Assets/Scripts/wolfScript.cs
+++ Assets/Scripts/wolfScript.cs
@@ -8,17 +8,26 @@ public class wolfScript : MonoBehaviour
     private Transform player;
     [SerializeField] private Animator animation;
     public float moveSpeed;
+    public float detectionDistance = 100f;
+    public float attackDistance = 20f;
     private static readonly int Moving = Animator.StringToHash("moving");
+    private static readonly int Attack = Animator.StringToHash("attack");
 
 
     // Update is called once per frame
     void Update()
     {
-        animation.SetBool(Moving, true);
         player = GameObject.FindWithTag("Player").transform;
-        transform.LookAt(player);
-        //transform.position = Vector3.Lerp(transform.position, player.position, 0.01f);
-        var direction = Vector3.forward;
-        transform.position += transform.rotation * direction * moveSpeed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, player.position) < attackDistance && !animation.GetCurrentAnimatorStateInfo(0).IsName("attack"))
+        {
+            animation.SetTrigger(Attack);
+        }
+        else if (Vector3.Distance(transform.position, player.position) < detectionDistance)
+        {
+            animation.SetBool(Moving, true);
+            transform.LookAt(player);
+            var direction = Vector3.forward;
+            transform.position += transform.rotation * direction * moveSpeed * Time.deltaTime;
+        }
     }
 }
