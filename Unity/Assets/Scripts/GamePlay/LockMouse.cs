dca323ab72d46aca0bda4136c7b7f853116dc018
unknown <lefortyop@gmail.com>
2020-02-26 18:24:00 +0100

    modification LockMouse

--- Unity/Assets/Scripts/GamePlay/LockMouse.cs
+++ Unity/Assets/Scripts/GamePlay/LockMouse.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LockMouse : MonoBehaviour
 {
- 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        
         
         if(Input.GetKeyDown(KeyCode.LeftAlt))
         {
