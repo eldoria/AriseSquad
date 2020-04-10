4336cf750daa9e02fdcc9be41d4dd500a048653a
eldoria <54.maxime.perez@gmail.com>
2020-04-10 16:34:18 +0200

    add build

--- Unity/Assets/Scripts/GamePlay/cameraController.cs
+++ Unity/Assets/Scripts/GamePlay/cameraController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Player;
+//using UnityEditor.Build.Player;
 using UnityEngine;
 
 public class cameraController : MonoBehaviour
