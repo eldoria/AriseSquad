23810f93972494ee12929bd09c0dc70d42f1e942
eldoria <54.maxime.perez@gmail.com>
2020-06-03 08:30:58 +0200

    fix bug camera during pause

--- Unity/Assets/Scripts/GamePlay/cameraController.cs
+++ Unity/Assets/Scripts/GamePlay/cameraController.cs
@@ -18,19 +18,17 @@ public class cameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-        
-        mouseX += rotationSpeed * Input.GetAxis("Mouse X");
-        mouseY -= rotationSpeed * Input.GetAxis("Mouse Y");
-        mouseY = Mathf.Clamp(mouseY, -60f, 30f);
-        
-        transform.LookAt(target);
-        target.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
 
-        if (Time.timeScale == 0)
-        {
-            mouseX = 0;
-            mouseY = 0;
-            transform.LookAt(target);
+        if (Time.timeScale != 0) {
+            mouseX += rotationSpeed * Input.GetAxis("Mouse X");
+            mouseY -= rotationSpeed * Input.GetAxis("Mouse Y");
+            mouseY = Mathf.Clamp(mouseY, -60f, 30f);
+        
+            //transform.LookAt(target);
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        }
+        else {
+            //transform.LookAt(target);
             target.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
         }
         
