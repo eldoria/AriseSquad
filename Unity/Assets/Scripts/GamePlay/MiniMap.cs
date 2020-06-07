03551525def18119cdc7b05ef7eb85ef47d395f0
Zukimotoji <lefortyop@gmail.com>
2020-06-08 00:22:01 +0200

    Ajout mini-map

---
+++ Unity/Assets/Scripts/GamePlay/MiniMap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+  public Transform Joueur_v5;
+  public Camera MainCamera;
+  public bool RotateWithPlayer = true;
+
+  private void Start()
+  {
+      SetPosition();
+      SetRotation();
+  }
+
+  private void LateUpdate()
+  {
+      if (Joueur_v5 != null)
+      {
+          SetPosition();
+          if (RotateWithPlayer && MainCamera)
+          {
+              SetRotation();
+          }
+      }
+  }
+
+  private void SetRotation()
+  {
+      transform.rotation = Quaternion.Euler(90.0f,MainCamera.transform.eulerAngles.y,0.0f);
+  }
+
+
+  private void SetPosition()
+  {
+      var newPos = Joueur_v5.position;
+      newPos.y = transform.position.y;
+
+      transform.position = newPos;
+  }
+}
