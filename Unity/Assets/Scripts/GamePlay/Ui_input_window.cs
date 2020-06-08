793bfb1143b4ca2d24bff98d4a3f3c424ab98eeb
eldoria <54.maxime.perez@gmail.com>
2020-06-08 14:03:18 +0200

    castleWithSqueletts

---
+++ Unity/Assets/Scripts/GamePlay/Ui_input_window.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ui_input_window : MonoBehaviour
+{
+   
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
