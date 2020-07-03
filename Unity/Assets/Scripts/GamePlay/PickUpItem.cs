010023a2e353719ed0e76baafb5f5a306951fdde
Zukimotoji <lefortyop@gmail.com>
2020-07-03 18:02:19 +0200

    Add SphereCast + destroy potion + interagir avec les popos

---
+++ Unity/Assets/Scripts/GamePlay/PickUpItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+
+    void PickUp()
+    {
+        Debug.Log("vous avez ramassé un potion HP");
+        Destroy(gameObject);
+    }
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
