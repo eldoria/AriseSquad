acef8c705e132caf0d3d98be86625d9d4619189e
Aid0z <mutheleteric@gmail.com>
2020-07-06 03:50:53 +0200

    Updated new game menu

---
+++ Unity/Assets/Scripts/NewGameScene/monsterCountScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class monsterCountScript : MonoBehaviour
+{
+    public Text monsterCountText;
+    public Scrollbar monsterCountScrollbar;
+    private float monsterCountValue = 0.15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewValue()
+    {
+        monsterCountValue = 0.1f + (0.2f * monsterCountScrollbar.value);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (monsterCountValue <= 0.105f)
+        {
+            monsterCountText.text = "Facile";
+        }
+        else if (monsterCountValue <= 0.1505f)
+        {
+            monsterCountText.text = "Normal";
+        }
+        else if(monsterCountValue <= 0.205f)
+        {
+            monsterCountText.text = "Difficile";
+        }
+        else if (monsterCountValue <= 0.2505f)
+        {
+            monsterCountText.text = "Monstrueux";
+        }
+        else
+        {
+            monsterCountText.text = "Ariser";
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/NewGameScene/vegetationScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vegetationScript : MonoBehaviour
+{
+    public Text vegetationText;
+    public Scrollbar vegetationScrollbar;
+    private float vegetationValue = 0.15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void NewValue()
+    {
+        vegetationValue = 0.1f + (0.2f * vegetationScrollbar.value);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (vegetationValue <= 0.105f)
+        {
+            vegetationText.text = "Faible";
+        }
+        else if (vegetationValue <= 0.1505f)
+        {
+            vegetationText.text = "Normale";
+        }
+        else if(vegetationValue <= 0.205f)
+        {
+            vegetationText.text = "Forte";
+        }
+        else if (vegetationValue <= 0.2505f)
+        {
+            vegetationText.text = "Dense";
+        }
+        else
+        {
+            vegetationText.text = "Ariser";
+        }
+    }
+
+}
