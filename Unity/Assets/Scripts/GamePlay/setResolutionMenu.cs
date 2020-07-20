4b0817df759c65b4113d39c98fed7fb08a703db1
eldoria <54.maxime.perez@gmail.com>
2020-07-20 21:38:00 +0200

    new script for fix error

---
+++ Unity/Assets/Scripts/GamePlay/setResolutionMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setResolutionMenu : MonoBehaviour
+{
+    
+    [SerializeField] private Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        int currentResolutionIndex = 0;
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].width == Screen.currentResolution.width)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetFullScreen(bool isFullEcran)
+    {
+        Screen.fullScreen = isFullEcran;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
+    }
+}
